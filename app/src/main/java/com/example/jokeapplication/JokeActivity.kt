9bea371c381a68933ff96808be7c95a540c3d873
Kitsune ^_^ <akito.merkulov@ya.ru>
2024-11-11 14:52:53 +0300

    Project refactored to fragments

---
+++ app/src/main/java/com/example/jokeapplication/JokeActivity.kt
@@ -0,0 +1,29 @@
+package com.example.jokeapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.jokeapplication.databinding.ActivityJokeBinding
+
+class JokeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityJokeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityJokeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        val navController = navHostFragment.navController
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        val navController = navHostFragment.navController
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+}
