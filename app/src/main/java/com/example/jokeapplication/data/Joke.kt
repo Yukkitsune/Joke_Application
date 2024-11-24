9f9d45f05c1fc16bc21ba7a64771a1595cd9f553
Kitsune ^_^ <akito.merkulov@ya.ru>
2024-11-24 21:53:12 +0300

    Users can add their own jokes now

--- app/src/main/java/com/example/jokeapplication/data/Joke.kt
+++ app/src/main/java/com/example/jokeapplication/data/Joke.kt
@@ -1,8 +1,10 @@
 package com.example.jokeapplication.data
 
 import androidx.annotation.DrawableRes
+import java.util.UUID;
 
 data class Joke(
+    val id: String = UUID.randomUUID().toString(),
     val category: String,
     val question: String,
     val answer: String,
--- app/src/main/java/com/example/jokeapplication/ui/joke_list/JokeListFragment.kt
+++ app/src/main/java/com/example/jokeapplication/ui/joke_list/JokeListFragment.kt
@@ -1,11 +1,11 @@
 package com.example.jokeapplication.ui.joke_list
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -14,21 +14,19 @@ import com.example.jokeapplication.R
 import com.example.jokeapplication.databinding.FragmentJokeListBinding
 import com.example.jokeapplication.ui.recycler.adapters.JokeAdapter
 import com.example.jokeapplication.ui.recycler.models.JokeViewModel
+import kotlinx.coroutines.launch
 
 
 class JokeListFragment : Fragment(R.layout.fragment_joke_list) {
 
     private val binding: FragmentJokeListBinding by viewBinding(FragmentJokeListBinding::bind)
-    private val jokeViewModel: JokeViewModel by viewModels()
+    private val jokeViewModel: JokeViewModel by activityViewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val adapter = JokeAdapter { joke ->
-            val action = JokeListFragmentDirections
-                .actionJokeListFragmentToJokeDetailsFragment(
-                    joke.category,
-                    joke.question,
-                    joke.answer
+            val action = JokeListFragmentDirections.actionJokeListFragmentToJokeDetailsFragment(
+                    joke.category, joke.question, joke.answer
                 )
             findNavController().navigate(action)
         }
@@ -37,8 +35,37 @@ class JokeListFragment : Fragment(R.layout.fragment_joke_list) {
         recyclerView.layoutManager = LinearLayoutManager(context)
         recyclerView.adapter = adapter
 
+        jokeViewModel.progressLiveData.observe(viewLifecycleOwner) { isLoading ->
+            binding.pbLoadingJokes.visibility = if (isLoading) View.VISIBLE else View.GONE
+            if (isLoading) {
+                binding.tvEmptyJokes.visibility = View.GONE
+                binding.ivEmptyJokes.visibility = View.GONE
+                recyclerView.visibility = View.GONE
+            }
+        }
+
         jokeViewModel.jokes.observe(viewLifecycleOwner) { jokes ->
-            adapter.submitList(jokes)
+            if (jokes.isNullOrEmpty()) {
+                binding.tvEmptyJokes.visibility = View.VISIBLE
+                binding.ivEmptyJokes.visibility = View.VISIBLE
+                binding.btnAddJoke.visibility = View.VISIBLE
+                recyclerView.visibility = View.GONE
+            } else {
+                binding.tvEmptyJokes.visibility = View.GONE
+                binding.ivEmptyJokes.visibility = View.GONE
+                recyclerView.visibility = View.VISIBLE
+                binding.btnAddJoke.visibility = View.VISIBLE
+                adapter.submitList(jokes)
+
+            }
+        }
+        viewLifecycleOwner.lifecycleScope.launch {
+            jokeViewModel.fetchJokes()
+        }
+        val btnAddJoke: Button = binding.btnAddJoke
+        btnAddJoke.setOnClickListener {
+            val action = JokeListFragmentDirections.actionJokeListFragmentToJokeAddFragment()
+            findNavController().navigate(action)
         }
 
     }
